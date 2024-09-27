ba535d2e5dd01187a55ccb1480a8858ba7d8519b
BenDev181 <benjiro181@gmail.com>
2024-09-27 16:28:17 +0100

    end of day 29/9/24

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+let expButton = document.querySelector("#exp-button");
+let expCounter = document.querySelector("#exp-counter");
+let expCount = 0;
+expButton.addEventListener("click", function(event) {
+    expCount++
+    expCounter.innerHTML = `${expCount} Years Coding Experience!`
+})
